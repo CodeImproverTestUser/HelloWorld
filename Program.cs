4feaff83fdc43e57679cd5179eba0b897713fe63
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-01-28 09:41:54 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+             CalculateClass cs = new CalculateClass(1,2,3,4,5,6,7,8,9);
+            Console.WriteLine("Sum() is:" + cs.Sum());
         }
     }
 }

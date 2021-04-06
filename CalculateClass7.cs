2b6824ff4a7fae06e7ab6786a817f394d5094c19
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-04-06 13:25:47 +0200

    Parameters reduced to 7
    Parent commit done with no app installed
    Child commit done with a fresh Duplication app

--- CalculateClass7.cs
+++ CalculateClass7.cs
@@ -4,10 +4,10 @@ namespace HelloWorld
 {
     public class CalculateClass
     {
-        int l1, l2, l3, l4, l5, l6, l7, l8, l9;
-        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+        int l1, l2, l3, l4, l5, l6, l7;
+        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7)
         {  
-            return(l1+l2+l3+l4+l5+l6+l7+l8+l9);
+            return(l1+l2+l3+l4+l5+l6+l7);
         }
     }
     
